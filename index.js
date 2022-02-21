61d417b2fbcdfd76fd64a263c52d0a47bcdfaffb
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-21 11:54:53 -0300

    Creating a connection to MongoDB using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_CONNECT_URL, err => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log('MONGO connected');
+    }
+})
 
 app.listen(process.env.PORT, error => {
     if(error) console.log(error);
