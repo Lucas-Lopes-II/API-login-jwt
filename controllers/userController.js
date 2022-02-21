bfea4ab3d161160f8e9208d1caa4ae9550736570
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-21 12:33:30 -0300

    Creating the login function on controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,3 +13,11 @@ const register = async (req, res) => {
         res.status(404).send(err);
     };
 };
+
+const login = async (req, res) => {
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if(!selectedUser) return res.status(400).send('Email or Password incorrect');
+    res.send('User logged')
+};
+
+module.exports = { register, login };
